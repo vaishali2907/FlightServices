cac887c6aa8b10c6c83fac0aa5900c8b7c8df7b6
vaishali2907 <70795947+vaishali2907@users.noreply.github.com>
2022-01-15 23:20:27 +0530

    Update settings.py

--- FlightServices/settings.py
+++ FlightServices/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 import config
