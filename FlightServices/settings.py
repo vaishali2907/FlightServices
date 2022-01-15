f4f139f42e5ed15cc3fffdb77958d458a3756094
vaishali2907 <70795947+vaishali2907@users.noreply.github.com>
2022-01-15 23:12:58 +0530

    Update settings.py
    removing secret key

--- FlightServices/settings.py
+++ FlightServices/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i1pe)2u2v(8(%&k1d&-i@dn6(w=x!s=b1z@i(qi-hc2y12l@$r'
+SECRET_KEY = 'secret key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
